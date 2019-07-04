6240d43c8ae69652b373263b5373cc9ec3f65880
Marc Goulding <gouldingmarc@gmail.com>
2019-07-05 00:14:25 +0100

    ema backtesting added

--- ema_crossover.py
+++ ema_crossover.py
@@ -1,31 +1,12 @@
 import configparser
 import pandas as pd
-import Binance
 parser = configparser.ConfigParser()
 parser.read("params.settings")
 
 data = pd.read_csv(parser.get('EMA', 'datapath'))
-data.columns = ['open time',
-                'open',
-                'high',
-                'low',
-                'close',
-                'volume',
-                'close time',
-                'asset volume',
-                '# trades',
-                'base buy volume',
-                'quote buy volume',
-                'ignore']
-data.pop('close time')
-data.pop('asset volume')
-data.pop('base buy volume')
-data.pop('quote buy volume')
-data.pop('ignore')
-# data columns: open, high, low, close, volume, # trades
-print("data fetched and cleaned")
+data.columns = ['open', 'high', 'low', 'close', 'volume', '# trades']
+print("data fetched")
 
-print(data[:100])
 price = data['close'].values
 length_ema1 = int(parser.get('EMA', 'ema1'))
 length_ema2 = int(parser.get('EMA', 'ema2'))
@@ -39,6 +20,15 @@ for candle in range(len(price)):
         greater.append(1)
     else:
         greater.append(0)
-# print(greater[:100])
-print(emaA[:100])
-print(emaB[:100])
+
+returns = 0
+for i in range(1,len(greater)):
+    if greater[i] == 1 and greater[i-1] == 0:
+        buy = price[i]
+        print("BUY - {}".format(buy))
+    elif greater[i] == 0 and greater[i-1] == 1:
+        return_ = (price[i]/buy)-1
+        print("SELL - {} === {}".format(price[i], return_))
+        returns += return_
+
+print(returns)
